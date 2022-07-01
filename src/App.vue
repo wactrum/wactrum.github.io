2382bab328ef9c781ec34a3dacea7d7010136bea
wactrum <patlakh.sergey@gmail.com>
2022-07-01 18:59:23 +0400

    Test pwa

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <router-view />
+</template>
+
+<script lang="ts">
+import {defineComponent, onMounted} from 'vue';
+import {useQuasar} from 'quasar';
+
+export default defineComponent({
+  name: 'App',
+
+  setup() {
+    const $q = useQuasar();
+    onMounted(() => {
+      console.warn();
+    })
+  }
+});
+</script>
